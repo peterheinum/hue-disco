e2f5db17aa08db972cfff2c9b155e0ff7b293c8e
peter heinum <peter.heinum@gmail.com>
2020-01-13 20:38:28 +0100

    started working on light reacting to beats

--- src/hue.js
+++ src/hue.js
@@ -1,23 +1,45 @@
 require('./spotify')
+const { get } = require('./utils')
 const { event_hub } = require('./eventhub')
 const { spin_light } = require('./app')
  
+const hue_hub = () => process.env.HUE_HUB
+const api_key = () => process.env.API_KEY
+
+
+const set_light = async ({ id, hue, bri }) => {
+  hue = Math.floor(hue)
+  bri = Math.floor(bri)
+
+  const url = `http://${hue_hub()}/api/${api_key()}/lights/${id}/state`
+  const body = { on: true, sat: 254, hue, bri }
+  const method = 'PUT'
+  const [status] = await get({ url, body, method })
+  console.log(status)
+  return Promise.resolve()
+}
+
 event_hub.on('tatums', tatums => { 
-  console.log({tatums})
+
 })
 
 event_hub.on('segments', segments => { 
-  console.log({segments})
+
 })
 
+let on = false
+
 event_hub.on('beats', beats => { 
-  console.log({beats})
+  for (let id = 0; id < 5; id++) {
+    set_light({id, hue: 5000, bri: on ? 100 : 150 })    
+  }
+  on = !on
 })
 
 event_hub.on('bars', bars => { 
-  console.log({bars})
+
 })
 
 event_hub.on('sections', sections => { 
-  console.log({sections})
+
 })
